1546fc228994428889d988607d7ead9d7146b20e
Specter <mikexx1264@gmail.com>
2020-09-02 01:32:15 -0500

    button/primary-secondary

--- components/stories/atoms/Button.stories.js
+++ components/stories/atoms/Button.stories.js
@@ -6,4 +6,5 @@ export default {
   component: Button,
 };
 
-export const Primary = () => <Button>Primary</Button>
+export const Primary = () => <Button type='primary'>Primary</Button>
+export const Secondary = () => <Button type='secondary'>Secondary</Button>
