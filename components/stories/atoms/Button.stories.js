060eec470b49479331e24fe343e5e5e000d88393
Specter <mikexx1264@gmail.com>
2020-09-01 22:26:36 -0500

    configuracion inicial storybook

---
+++ components/stories/atoms/Button.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Button from '../../atomic/atoms/Button';
+
+export default {
+  title: 'Atoms/Buttons',
+  component: Button,
+};
+
+export const Primary = () => <Button>Primary</Button>
